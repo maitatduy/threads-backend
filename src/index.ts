24f4e4e523edaa4f9f98973a56f9367d42233434
maitatduy <maitatduy.work@gmail.com>
2026-05-11 10:50:35 +0700

    chore: setup mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-console.log("Threads API");
+import dns from "node:dns";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+import "dotenv/config";
+
+import express from "express";
+import databaseService from "@/services/database.service";
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Threads API");
+});
+
+async function startServer() {
+    try {
+        await databaseService.connect();
+
+        console.log("Đã kết nối database thành công!");
+
+        app.listen(PORT, () => {
+            console.log(`Server đang chạy ở cổng ${PORT}`);
+        });
+    } catch (error) {
+        console.log("Kết nối database thất bại!");
+        console.log(error);
+    }
+}
+
+startServer();
---
+++ src/services/database.service.ts
@@ -0,0 +1,36 @@
+import { MongoClient, Db, Collection } from "mongodb";
+
+const uri = process.env.MONGODB_URI as string;
+
+class DatabaseService {
+    private client: MongoClient;
+    private db: Db;
+
+    constructor() {
+        this.client = new MongoClient(uri);
+        this.db = this.client.db("threads");
+    }
+
+    async connect() {
+        try {
+            await this.client.connect();
+
+            await this.db.command({
+                ping: 1,
+            });
+
+            console.log("Kết nối MongoDB thành công!");
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    get users(): Collection {
+        return this.db.collection("users");
+    }
+}
+
+const databaseService = new DatabaseService();
+
+export default databaseService;
